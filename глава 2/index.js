e3e32bb95725762714bdc04313e8d95f7f3f5e3e
Volkov Nikolay <air__nike_9@outlook.com>
2023-10-06 17:20:04 +0300

    +

--- глава 2/index.js
+++ глава 2/index.js
@@ -1,4 +1,6 @@
+var destination = document.querySelector("#container");
+
 ReactDOM.render(
-    <h1>Hi NIK and React!</h1>,
-    document.body
+  <h1>Hi Batman and React!</h1>,
+  destination //destination === document.querySelector('#container')
 );
