4c81d09d07b81dbc7a5f1668125e4a1ac6424cb2
Nik <airgit1991@rambler.ru>
2023-10-07 09:28:11 +0300

    +

--- глава 2/index.js
+++ глава 2/index.js
@@ -1,6 +1,25 @@
+/*
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
-  <h1>Hi Batman and React!</h1>,
-  destination //destination === document.querySelector('#container')
+  <div>
+    <h3><i>Hi Batman</i></h3>
+    <h3><i>Hi Jocker</i></h3>
+    <h3><i>Hi Dobrinya</i></h3>
+    <h3><i>Hi Erast</i></h3>
+  </div>,
+  
+  destination 
 );
+*/  
+
+class HelloWorld extends React.Component{
+  render(){
+    return <p>Hello, components world!</p>
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.querySelector('#container')
+)
